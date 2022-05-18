8dad57d2c6d97014144aad2f9f692757119944e9
alexkarunny <smetan4ik1@gmail.com>
2022-05-18 23:44:48 +0300

    add: Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const addName = (name: string) => {
+        console.log(`Hello ${name}!`)
+    }
+
+    return (
+        <div className="App">
+            <Button title={'Hello'} callback={() => addName('Alex')}/>
+        </div>
+    );
 }
 
 export default App;
